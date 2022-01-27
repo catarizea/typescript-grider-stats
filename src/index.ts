d55c1388abe8264bf93abdc014d76a90f14552c8
Catalin Rizea <catalinrizea1@gmail.com>
2022-01-27 17:35:08 +0200

    csv file reader with tuple for a row

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import { CsvFileReader } from "./CsvFileReader";
+import { MatchResult } from "./MatchResult";
+
+const reader = new CsvFileReader('soccer.csv');
+reader.read();
+
+let manUnitedWins = 0;
+
+for (let row of reader.data) {
+  if (row[1] === 'Man United' && row[5] === MatchResult.HomeWin) {
+    manUnitedWins++;
+  } else if (row[2] === 'Man United' && row[5] === MatchResult.AwayWin) {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Man United won ${manUnitedWins} games`);
