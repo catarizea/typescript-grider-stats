1a4f61c06e08ab953d1099b4ca2a51b9d0416193
Catalin Rizea <catalinrizea1@gmail.com>
2022-02-01 21:03:40 +0200

    abstract class and generic

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import { CsvFileReader } from "./CsvFileReader";
+import { MatchReader } from "./MatchReader";
 import { MatchResult } from "./MatchResult";
 
-const reader = new CsvFileReader('soccer.csv');
+const reader = new MatchReader('soccer.csv');
 reader.read();
 
 let manUnitedWins = 0;
